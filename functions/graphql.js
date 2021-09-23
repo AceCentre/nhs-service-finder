e55493edc8d20338ed94586ff454907f313b3e7a
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-09-23 09:46:40 +0100

    Trying gateway 1.0

--- functions/graphql.js
+++ functions/graphql.js
@@ -32,7 +32,7 @@ const server = new ApolloServer({
 // For some reason 'apollo-server-lambda' needs extra data
 // its a hack and might not be correct but it works.
 // We merge this with the real event, the real event takes precedence.
-const MANUAL_EVENT_PARAMS = { Records: true };
+const MANUAL_EVENT_PARAMS = { requestContext: true, version: "1.0" };
 
 exports.handler = (event, context) => {
   const newEvent = { ...MANUAL_EVENT_PARAMS, ...event };
