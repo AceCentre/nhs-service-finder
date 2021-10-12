02b3becb9d0e70c7757fcb5d6adf2755f2269466
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-10-12 14:32:10 +0100

    Add a priority list of where to get the data from
    Remove symlink
    
    Add more data
    
    Add a specific file for each service type
    
    Make files smaller
    
    Make files small

--- scripts/create-geo-json.js
+++ scripts/create-geo-json.js
@@ -1,80 +1,92 @@
 const { services } = require("../data/services.json");
 const fs = require("fs");
 const path = require("path");
-const ccgGeoData = require("../archive/Clinical_Commissioning_Groups_(April_2019).json");
-const olderCcgGeoData = require("../archive/Clinical_Commissioning_Groups_(April_2016)_Boundaries.json");
 
 const geojsonhint = require("@mapbox/geojsonhint");
 
-let features = [];
+const writeGeoJsonForServices = (currentServiceList, outputName) => {
+  let features = [];
 
-// Start with only AAC services for simplicity
-// const aacServices = services.filter((service) =>
-//   service.servicesOffered.includes("aac")
-// );
+  const CcgEngland21 = require("../archive/Clinical_Commissioning_Groups_(April_2021)_EN_BUC.json");
+  const CcgEngland20 = require("../archive/Clinical_Commissioning_Groups_(April_2020)_EN_BFC_V2.json");
+  const CcgEngland19 = require("../archive/Clinical_Commissioning_Groups_(April_2019).json");
+  const CcgEngland17 = require("../archive/Clinical_Commissioning_Groups_(April_2017)_Boundaries_(Version_4).json");
+  const CcgEngland16 = require("../archive/Clinical_Commissioning_Groups_(April_2016)_Boundaries.json");
+  const CcgEngland15 = require("../archive/Clinical_Commissioning_Groups_(July_2015)_Boundaries.json");
 
-for (const currentService of services) {
-  let coordinateGroups = [];
+  const PRIORITY_ORDER_GEOJSON = [
+    { map: CcgEngland21, key: "CCG21CD" },
+    { map: CcgEngland20, key: "ccg20cd" },
+    { map: CcgEngland19, key: "ccg19cd" },
+    { map: CcgEngland17, key: "ccg17cd" },
+    { map: CcgEngland16, key: "ccg16cd" },
+    { map: CcgEngland15, key: "ccg15cd" },
+  ];
 
-  for (const currentCcg of currentService.ccgCodes) {
-    let featureForCcg = ccgGeoData.features.find((x) => {
-      return x.properties.ccg19cd.toLowerCase() === currentCcg.toLowerCase();
-    });
+  for (const currentService of currentServiceList) {
+    let coordinateGroups = [];
 
-    // Look in older data if we cant find the answer in newer data
-    if (!featureForCcg) {
-      featureForCcg = olderCcgGeoData.features.find((x) => {
-        return x.properties.ccg16cd.toLowerCase() === currentCcg.toLowerCase();
-      });
-    }
+    for (const currentCcg of currentService.ccgCodes) {
+      let featureForCcg = null;
 
-    if (featureForCcg) {
-      if (featureForCcg.geometry.type === "MultiPolygon") {
-        featureForCcg.geometry.coordinates.forEach((coordinateGroup) => {
-          coordinateGroups.push(coordinateGroup);
-        });
-      } else if ("Polygon") {
-        coordinateGroups.push(featureForCcg.geometry.coordinates);
-      } else {
-        throw new Error("We dont know how to deal with that");
+      for (const currentSource of PRIORITY_ORDER_GEOJSON) {
+        if (!featureForCcg) {
+          featureForCcg = currentSource.map.features.find((x) => {
+            return (
+              x.properties[currentSource.key].toLowerCase() ===
+              currentCcg.toLowerCase()
+            );
+          });
+        }
       }
-    } else {
-      console.log(`Could not find location for ccg, ${currentCcg}`);
-      // throw new Error(`Could not find location for ccg, ${currentCcg}`);
+
+      if (featureForCcg) {
+        if (featureForCcg.geometry.type === "MultiPolygon") {
+          featureForCcg.geometry.coordinates.forEach((coordinateGroup) => {
+            coordinateGroups.push(coordinateGroup);
+          });
+        } else if ("Polygon") {
+          coordinateGroups.push(featureForCcg.geometry.coordinates);
+        } else {
+          throw new Error("We dont know how to deal with that");
+        }
+      } else {
+        console.log(`Could not find location for ccg, ${currentCcg}`);
+        // throw new Error(`Could not find location for ccg, ${currentCcg}`);
+      }
     }
+
+    let currentFeature = {
+      type: "Feature",
+      properties: { serviceId: currentService.id },
+      geometry: {
+        type: "MultiPolygon",
+        coordinates: coordinateGroups,
+      },
+    };
+
+    features.push(currentFeature);
   }
 
-  let currentFeature = {
-    type: "Feature",
-    properties: { serviceId: currentService.id },
-    geometry: {
-      type: "MultiPolygon",
-      coordinates: coordinateGroups,
-    },
-  };
+  const result = { type: "FeatureCollection", features };
 
-  features.push(currentFeature);
-}
+  console.log(geojsonhint.hint(JSON.stringify(result, null, 2)));
 
-const result = { type: "FeatureCollection", features };
+  fs.writeFileSync(
+    path.join(__dirname, "../data/", outputName),
+    JSON.stringify(result, null, 2)
+  );
+};
 
-console.log(geojsonhint.hint(JSON.stringify(result, null, 2)));
-
-fs.writeFileSync(
-  path.join(__dirname, "../data/services-geo.json"),
-  JSON.stringify(result, null, 2)
+const aac = services.filter((service) =>
+  service.servicesOffered.includes("aac")
+);
+const ec = services.filter((service) => service.servicesOffered.includes("ec"));
+const wcs = services.filter((service) =>
+  service.servicesOffered.includes("wcs")
 );
 
-// const turf = require("@turf/turf");
-
-// // AceCentre location
-// // @53.5166219,-2.1425253
-// const currentPoint = turf.point([-2.1425253, 53.5166219]);
-// let featuresWithPoints = [];
-// for (const current of features) {
-//   const multiPolygon = turf.multiPolygon(current.geometry.coordinates);
-//   const ptsWithin = turf.pointsWithinPolygon(currentPoint, multiPolygon);
-//   if (ptsWithin.features.length > 0) {
-//     featuresWithPoints.push(current.properties);
-//   }
-// }
+writeGeoJsonForServices(aac, "aac-services-geo.geojson");
+writeGeoJsonForServices(ec, "ec-services-geo.geojson");
+writeGeoJsonForServices(wcs, "wcs-services-geo.geojson");
+writeGeoJsonForServices(services, "services-geo.json");
