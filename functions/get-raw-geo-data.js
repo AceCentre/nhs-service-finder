0a30a17451f3fc756637b121055edc713e5074e2
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2021-10-15 12:18:11 +0100

    Write basic function

---
+++ functions/get-raw-geo-data.js
@@ -0,0 +1,54 @@
+const fs = require("fs").promises;
+const path = require("path");
+
+let aacServicesGeo = null;
+let ecServicesGeo = null;
+let wcsServicesGeo = null;
+
+const getServicesGeo = async () => {
+  // Return the files if we have already done the work to get them
+  if (aacServicesGeo && ecServicesGeo && wcsServicesGeo) {
+    return {
+      aac: aacServicesGeo,
+      ec: ecServicesGeo,
+      wcs: wcsServicesGeo,
+    };
+  }
+
+  const dataFolder = path.join(__dirname, "../data");
+
+  const results = await Promise.all([
+    fs.readFile(path.join(dataFolder, "./aac-services-geo.geojson")),
+    fs.readFile(path.join(dataFolder, "./ec-services-geo.geojson")),
+    fs.readFile(path.join(dataFolder, "./wcs-services-geo.geojson")),
+  ]);
+
+  aacServicesGeo = JSON.parse(results[0].toString());
+  ecServicesGeo = JSON.parse(results[1].toString());
+  wcsServicesGeo = JSON.parse(results[2].toString());
+
+  return {
+    aac: aacServicesGeo,
+    ec: ecServicesGeo,
+    wcs: wcsServicesGeo,
+  };
+};
+
+exports.handler = async ({ queryStringParameters }, context) => {
+  const data = await getServicesGeo();
+
+  if (
+    !queryStringParameters ||
+    !queryStringParameters.type ||
+    !data[queryStringParameters.type.toLowerCase()]
+  ) {
+    return {
+      statusCode: 404,
+    };
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(data[queryStringParameters.type.toLowerCase()]),
+  };
+};
